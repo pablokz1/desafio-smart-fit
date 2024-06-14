1a332209cecc082a306dd7e4950dd9fa021e1b92
pablokz1 <pablo.carvalho16@hotmail.com>
2024-06-13 22:23:04 -0300

    feat: adicionado form control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    FormsComponent,
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
