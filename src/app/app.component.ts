fa16b2d5fe700a4318dda89c3b24b265752c8c8d
Pablokz1 <pablo.carvalho16@hotmail.com>
2024-08-08 16:40:05 -0300

    migrate for standalone components stap 2 and 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Locations } from './types/locations.interface';
 import { GetUnitsService } from './services/get-units.service';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { NgIf, AsyncPipe } from '@angular/common';
+import { LegendsComponent } from './components/legends/legends.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [HeaderComponent, FormsComponent, LegendsComponent, NgIf, CardsListComponent, FooterComponent, AsyncPipe]
 })
 export class AppComponent {
   showList = new BehaviorSubject<boolean>(false);
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { CommonModule } from '@angular/common';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, ReactiveFormsModule, CommonModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
